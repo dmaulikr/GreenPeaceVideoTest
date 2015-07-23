c8f1180ee2cc7473ee81c04ca67b8995f5e37e2b
Alexander Makarov <magrus87@MacBook-Pro-Alexander.local>
2015-07-23 23:21:24 +0500

    Initial Commit

---
+++ GreenPeaceVideoTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GreenPeaceVideoTest
+//
+//  Created by Alexander Makarov on 23.07.15.
+//  Copyright (c) 2015 Alexander Makarov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GreenPeaceVideoTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GreenPeaceVideoTest
+//
+//  Created by Alexander Makarov on 23.07.15.
+//  Copyright (c) 2015 Alexander Makarov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
